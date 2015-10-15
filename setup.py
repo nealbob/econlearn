f8f857df8b5a781b9a35b45c3f43377ded266567
Neal Hughes <nealbob@nealbob-MacBook.(none)>
2015-10-15 21:06:42 +1100

    fix

--- setup.py
+++ setup.py
@@ -9,7 +9,9 @@ ext_modules=[
     Extension("tilecode",
               ["tilecode.pyx"],
               libraries=["m"],
-              extra_compile_args = ["-O3", "-ffast-math", "-march=native"],
+              extra_compile_args = ["-O3", "-ffast-math", "-march=native", "-fopenmp"], #, "-std=c++11"
+              include_dirs = [np.get_include(),"..", "../include"], #, cython_gsl.get_cython_include_dir()],
+              extra_link_args=['-fopenmp'],
               ) 
 ]
 
